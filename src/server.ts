22d1c4564931c853ac7079b0ecd23b0a757a94db
Provash <provash20cb@gmail.com>
2026-06-02 16:59:19 +0600

    7-2,3 Express & Setting Up Postgres with Neon Serverless Cloud

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,38 @@
-import express from "express";
+import express, { type Application, type Request, type Response } from "express";
+import {Pool} from "pg"
+const app : Application = express()
+const port = 5000
 
-const app = express()
-const port = 3000
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended : true}))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const pool = new Pool({
+    connectionString:"postgresql://neondb_owner:npg_SYnBF1sr9ibM@ep-nameless-sound-ap9q634f-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
+
+// app.get('/', (req : Request, res : Response) => {
+//   res.send('Mission 2 - Be a Node Expert. 7,8,9 Practice !')
+// })
+
+app.get('/', (req : Request, res : Response) => {
+//   res.send('Mission 2 - Be a Node Expert. 7,8,9 Practice !')
+res.status(200).json({
+    message : "Express Server",
+    "author" : " Next Level"
+})
+})
+
+app.post('/',async(req: Request, res: Response)=>{
+    // console.log(req.body);
+    // const body = req.body; //body destructure
+    const {name, email,password} = req.body;
+    res.status(201).json({
+        message : "Created",
+        data: {
+            name, email
+        },
+    })
 })
 
 app.listen(port, () => {
