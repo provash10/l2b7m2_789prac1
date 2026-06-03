d1f812e917189252b52ce94e4dce7088ebf61134
Provash <provash20cb@gmail.com>
2026-06-03 16:24:32 +0600

    7-6 Creating our first User with POST method

--- src/server.ts
+++ src/server.ts
@@ -16,9 +16,9 @@ const initDB=async()=>{
         await pool.query(`
             CREATE TABLE IF NOT EXISTS users(
             id SERIAL PRIMARY KEY,
-            name VARCHAR(20),
-            email VARCHAR(20) NOT NULL,
-            password VARCHAR(20) NOT NULL,
+            name VARCHAR(100),
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
             is_active BOOLEAN DEFAULT true,
             age INT,
 
@@ -49,14 +49,27 @@ res.status(200).json({
 app.post('/',async(req: Request, res: Response)=>{
     // console.log(req.body);
     // const body = req.body; //body destructure
-    const {name, email,password} = req.body;
+    const {name, email,password,age} = req.body;
+
+   try {
+     const result = await pool.query(`
+        INSERT INTO users(name,email,password,age) VALUES($1,$2,$3,$4)
+        RETURNING *
+        `,[name,email,password,age]
+    );
+        // console.log(result);
+
     res.status(201).json({
-        message : "Created",
-        data: {
-            name, email
-        },
-    })
-})
+        message : "User Created Successfully",
+        data: result.rows[0],
+    });
+   } catch (error:any) {
+    res.status(500).json({
+        message : error.message,
+        error: error,
+    });
+   }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
