ea1d2f1a1b56d0d9d651887d95609b177d91e14d
Provash <provash20cb@gmail.com>
2026-06-03 15:26:16 +0600

    7-5 Executing Pool and creating tables

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,31 @@ app.use(express.text());
 app.use(express.urlencoded({extended : true}))
 
 const pool = new Pool({
-    connectionString:"postgresql://neondb_owner:npg_SYnBF1sr9ibM@ep-nameless-sound-ap9q634f-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+    connectionString:"postgresql://neondb_owner:npg_dK1LytpgYv8z@ep-bold-pond-aqk2siik-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 })
+
+const initDB=async()=>{
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(20),
+            email VARCHAR(20) NOT NULL,
+            password VARCHAR(20) NOT NULL,
+            is_active BOOLEAN DEFAULT true,
+            age INT,
+
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
+            `)
+            console.log("Database Connected Successfully")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+initDB();
 
 // app.get('/', (req : Request, res : Response) => {
 //   res.send('Mission 2 - Be a Node Expert. 7,8,9 Practice !')
