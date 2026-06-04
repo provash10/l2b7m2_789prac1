d714ac9324137b977369a51f98c76c26879cb961
Provash <provash20cb@gmail.com>
2026-06-04 19:46:08 +0600

    8-8 Introduction with Bcrypt and hashing password

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,17 +1,21 @@
 import { pool } from "../../db";
 import type { IUSER } from "./user.interface";
+import bcrypt from "bcryptjs";
 
 const createUserIntoDB=async(payload : IUSER)=>{
     const{name, email, password, age} = payload;
+
+    const hashPassword = await bcrypt.hash(password,10);
 
     const result = await pool.query(
       `
         INSERT INTO users(name,email,password,age) VALUES($1,$2,$3,$4)
         RETURNING *
         `,
-      [name, email, password, age]
+      [name, email, hashPassword, age]
     );
     // console.log(result);
+    delete result.rows[0].password;
     return result;
 }
 
