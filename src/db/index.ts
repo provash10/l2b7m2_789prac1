569eefab81fae9a2dd5fc06afba5b1403089d8af
Provash <provash20cb@gmail.com>
2026-06-04 16:31:52 +0600

    8-5 Managing the user profile with SQL reference

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 import config from "../config";
 
 export const pool = new Pool({
-  connectionString: config.connection_string
+  connectionString: config.connection_string,
 });
 
 export const initDB = async () => {
@@ -20,6 +20,21 @@ export const initDB = async () => {
             updated_at TIMESTAMP DEFAULT NOW()
             )
             `);
+
+            await pool.query(`
+              CREATE TABLE IF NOT EXISTS profiles(
+              id SERIAL PRIMARY KEY,
+              user_id INT UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+              bio TEXT,
+              address TEXT,
+              phone VARCHAR(15),
+              gender VARCHAR(10),
+
+              created_at TIMESTAMP DEFAULT NOW(),
+              updated_at TIMESTAMP DEFAULT NOW()
+              )
+              `);
+
     console.log("Database Connected Successfully");
   } catch (error) {
     console.log(error);
