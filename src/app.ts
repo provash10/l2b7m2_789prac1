8dd92d09e180bd86b0126872a8ec6d2108d6b503
Provash <provash20cb@gmail.com>
2026-06-06 22:29:09 +0600

    9-2 Creation auth middleware auth.ts token send

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,8 @@ import { pool } from "./db";
 import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { authRouter } from "./modules/auth/auth.route";
-import fs from 'fs';
+import logger from "./middleware/logger";
+
 
 
 const app: Application = express();
@@ -19,14 +20,7 @@ app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
 //express middleware
-app.use((req, res, next) => {
-  console.log('Method - URL - Time:', req.method, req.url, Date.now());
-  const log = (`\nMethod -> ${req.method} - Time -> ${Date.now()} - URL -> ${req.url}\n`);
-  fs.appendFile('logger.txt', log, (err)=>{
-    console.log(err);
-  })
-  next();
-});
+app.use(logger);
 
 app.get("/", (req: Request, res: Response) => {
   //   res.send('Mission 2 - Be a Node Expert. 7,8,9 Practice !')
