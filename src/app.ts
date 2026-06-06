800480e93193bfcd363bbbe0edfb1fa994a97d3c
Provash <provash20cb@gmail.com>
2026-06-06 21:07:11 +0600

    9-1 Understanding express middleware create logger.txt

--- src/app.ts
+++ src/app.ts
@@ -9,12 +9,24 @@ import { pool } from "./db";
 import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { authRouter } from "./modules/auth/auth.route";
+import fs from 'fs';
+
+
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
+//express middleware
+app.use((req, res, next) => {
+  console.log('Method - URL - Time:', req.method, req.url, Date.now());
+  const log = (`\nMethod -> ${req.method} - Time -> ${Date.now()} - URL -> ${req.url}\n`);
+  fs.appendFile('logger.txt', log, (err)=>{
+    console.log(err);
+  })
+  next();
+});
 
 app.get("/", (req: Request, res: Response) => {
   //   res.send('Mission 2 - Be a Node Expert. 7,8,9 Practice !')
