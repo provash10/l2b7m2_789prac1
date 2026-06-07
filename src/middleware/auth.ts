e07f12294f5773fcc808ab43211996971a995374
Provash <provash20cb@gmail.com>
2026-06-07 16:36:58 +0600

    9-3 Auth middleware validation token, decoded-user,user check by DB

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,10 +1,15 @@
 import type { NextFunction, Request, Response } from "express";
+import jwt, { type JwtPayload } from 'jsonwebtoken';
+import config from "../config";
+import { pool } from "../db";
 
 const auth = ()=>{
     return async (req:Request, res: Response, next: NextFunction)=>{
     // console.log("This is Protected Route");
     // console.log(req.headers.authorization);
     const token = req.headers.authorization;
+    // console.log(token)
+    
     if(!token){
         res.status(401).json({
       success: false,
@@ -12,6 +17,33 @@ const auth = ()=>{
       data: {},
     });
     }
+
+    const decoded =jwt.verify(token as string, config.secret as string) as JwtPayload;
+
+    // console.log(decoded)
+    const userData = await pool.query(
+      `
+      SELECT * FROM users WHERE email=$1
+      `,[decoded.email],
+    )
+
+    // console.log(userData);
+    const user = userData.rows[0]
+    
+    if(userData.rows.length===0){
+      res.status(404).json({
+        success:false,
+        message:"User not found !",
+      });
+    }
+
+    if (!user.is_active){
+       res.status(403).json({
+        success:false,
+        message:"Forbidden !",
+      });
+    }
+
     next();
 }
 }
